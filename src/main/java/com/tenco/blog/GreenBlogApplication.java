644cf50302984f75358871dc061df22e7fbef1fb
wjdekdnsekdns <ekdns8276@naver.com>
2023-05-09 10:19:08 +0900

    프로젝트 기본 설정

---
+++ src/main/java/com/tenco/blog/GreenBlogApplication.java
@@ -0,0 +1,13 @@
+package com.tenco.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenco/blog/GreenBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenco.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
